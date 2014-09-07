45f6234b944c32613ceda977a7323f68eddd8c24
Kyle Schiller <schillerk@carleton.edu>
2014-09-07 16:29:40 -0500

    add a comment

--- Menu.java
+++ Menu.java
@@ -13,6 +13,7 @@ class Menu
 
 	public void startGame()
 	{
+		/// This comment is meaningless
 		System.out.print("Enter your name: ");
 		String pName = keyboard.next();
 		String[] i = new String[8];
